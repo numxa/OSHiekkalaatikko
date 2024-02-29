1e9c9a2a41f925ff152bd479afece229be26d74e
numxa <olli.numminen@hotmail.com>
2024-02-29 13:48:55 +0200

    Add rudimentary terminal scrolling

--- kernel.c
+++ kernel.c
@@ -73,6 +73,29 @@ void terminal_initialize(void)
 		}
 	}
 }
+
+void terminal_scroll_up(void)
+{
+	for (size_t y = 1; y < VGA_HEIGHT; y++) 
+    {
+		for (size_t x = 0; x < VGA_WIDTH; x++) 
+        {
+			const size_t dest = (y-1) * VGA_WIDTH + x;
+			const size_t src = y * VGA_WIDTH + x;
+			terminal_buffer[dest] = terminal_buffer[src];
+		}
+	}
+}
+
+void terminal_clear_bottom(void)
+{
+	size_t y = 24;
+	for (size_t x = 0; x < VGA_WIDTH; x++)
+	{
+		const size_t index = y * VGA_WIDTH + x;
+		terminal_buffer[index] = vga_entry(' ', terminal_color);
+	}
+}
  
 void terminal_setcolor(uint8_t color) 
 {
@@ -87,10 +110,16 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
  
 void terminal_putchar(char c) 
 {
+	/*Newline support*/
     if (c == '\n')
     {
-        ++terminal_row;
         terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+		{
+			terminal_scroll_up();
+			--terminal_row;
+			terminal_clear_bottom();
+		}
     }
     else 
     {
@@ -98,8 +127,10 @@ void terminal_putchar(char c)
 	    if (++terminal_column == VGA_WIDTH)
         {
 		    terminal_column = 0;
-		    if (++terminal_row == VGA_HEIGHT)
-			    terminal_row = 0;
+		    if (++terminal_row == VGA_HEIGHT - 1)
+			{
+				terminal_scroll_up();
+			}	
         }
 	}
 }
@@ -120,7 +151,11 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
  
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
-    terminal_writestring("This is the second line!");
+	for (int i = 0; i < 24; i++)
+	{
+		terminal_writestring("Hello, kernel World!\n");
+	}
+	terminal_writestring("TESTI!\n");
+
+	
 }
